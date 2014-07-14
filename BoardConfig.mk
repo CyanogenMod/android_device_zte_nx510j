853e104d6687018322f18ff36aa6299e3793b5fc
Jeykumar Sankaran <jsanka@codeaurora.org>
2014-07-14 16:12:08 -0700

    msm8994: Enable triple framebuffer for 8994
    Enable triple framebuffer for 8994.
    
    Change-Id: I548ecaed4225deb6c9276ecbf9f0e6754d8981e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,7 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a9
 
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 TARGET_NO_BOOTLOADER := false
 TARGET_NO_KERNEL := false
 -include $(QCPATH)/common/msm8994/BoardConfigVendor.mk
