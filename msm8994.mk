b8336a2ca55aedb70d9b6ca881b384e032086340
Mahesh Lanka <mlanka@codeaurora.org>
2014-07-30 22:39:36 +0530

    msm8994: Added extendedmediaplayer to PRODUCT_BOOT_JARS
    Added extendedmediaplayer to PRODUCT_BOOT_JARS
    
    Change-Id: I7f9644ab1d6f017b95e94f0652e5d995944d6cd4

--- msm8994.mk
+++ msm8994.mk
@@ -31,6 +31,8 @@ PRODUCT_BOOT_JARS += qcmediaplayer \
                      qcom.fmradio
 
 PRODUCT_BOOT_JARS += WfdCommon
+PRODUCT_BOOT_JARS += extendedmediaextractor
+
 # default is nosdcard, S/W button enabled in resource
 PRODUCT_CHARACTERISTICS := nosdcard
 
