71f93cf7bf5d0fa59d0be2166074b6c6604a863b
Arun Kumar K.R <akumarkr@codeaurora.org>
2014-07-30 20:30:41 -0700

    msm8994: set TARGET_USES_QCOM_BSP true
    This flag enables the QCOM specific display features
    
    Change-Id: I60a5ac7afbf192d8673609b51e5ab14c80e4ede1

--- msm8994.mk
+++ msm8994.mk
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+TARGET_USES_QCOM_BSP := true
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
 # copy customized media_profiles and media_codecs xmls for 8994
