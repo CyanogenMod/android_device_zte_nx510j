8193b27732bd027ab96a1f2b7643938faab2d428
Mingming Yin <mingming@codeaurora.org>
2014-08-15 14:46:11 -0700

    audio: load audio_policy.conf based on AOSP flag
    - Load default audio_policy.conf from device/qcom/common
      for PDK build
    - Load extended audio_policy.conf from target folder
      to supported extended audio features
    
    Change-Id: I3f8789c70ef99350fb17a0e7856341b2eeaaf39f

--- msm8994.mk
+++ msm8994.mk
@@ -43,8 +43,15 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_PACKAGES += libGLES_android
 
 # Audio configuration file
+ifeq ($(TARGET_USES_AOSP), true)
 PRODUCT_COPY_FILES += \
-    device/qcom/msm8994/audio_policy.conf:system/etc/audio_policy.conf \
+    device/qcom/common/media/audio_policy.conf:system/etc/audio_policy.conf
+else
+PRODUCT_COPY_FILES += \
+    device/qcom/msm8994/audio_policy.conf:system/etc/audio_policy.conf
+endif
+
+PRODUCT_COPY_FILES += \
     device/qcom/msm8994/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     device/qcom/msm8994/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     device/qcom/msm8994/mixer_paths.xml:system/etc/mixer_paths.xml \
