b06f26ce83b94ed36a325c75c8e4fe503ba867ac
dianlujitao <dianlujitao@gmail.com>
2015-07-08 13:41:30 +0800

    nx510j: Set Qualcomm flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,9 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 #is controlling the power on/off on certain peripherals.
 TARGET_PER_MGR_ENABLED := true
 
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+
 # Ril
 # Added to indicate that protobuf-c is supported in this build
 PROTOBUF_SUPPORTED := true
