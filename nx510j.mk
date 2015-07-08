d10f1aba0dafb75fb965abeee557f64885ff867c
dianlujitao <dianlujitao@gmail.com>
2015-07-08 14:34:49 +0800

    Derp

--- nx510j.mk
+++ nx510j.mk
@@ -48,7 +48,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/listen_platform_info.xml:system/etc/listen_platform_info.xml
 
-PRODUCT_COPY_FILES := \
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:system/etc/permissions/android.hardware.camera.full.xml\
