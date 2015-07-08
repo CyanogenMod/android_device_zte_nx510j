0011ef4a18dcb949958700101416aacc9316bdc0
dianlujitao <dianlujitao@gmail.com>
2015-07-08 13:56:00 +0800

    nx510j: Set fstab for recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -148,6 +148,9 @@ TARGET_POWERHAL_VARIANT := qcom
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
 
+# Recovery
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+
 # Ril
 # Added to indicate that protobuf-c is supported in this build
 PROTOBUF_SUPPORTED := true
