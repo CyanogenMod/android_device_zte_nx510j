fe35cb10d44dd83128399fad5a98951142bc640b
dianlujitao <dianlujitao@gmail.com>
2015-07-15 16:22:20 +0800

    nx510j: Add missing wlan_mac.bin symlink

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -91,6 +91,8 @@ $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
         $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_wlan_nv.bin; \
         ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
         $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
+        ln -sf /persist/wlan_mac.bin \
+        $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin; \
         ln -sf /dev/block/bootdevice/by-name/msadp \
         $(TARGET_OUT_ETC)/firmware/msadp )
 endif
