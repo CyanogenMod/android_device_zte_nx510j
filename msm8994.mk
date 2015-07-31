a757d14661e894cc9bfd54486f04fa0e8fda1e57
Divya Sharma <c_shard@codeaurora.org>
2015-07-30 17:57:58 -0700

    msm8994: Add oem-services to bootclasspath
    Add oem-services to support Diag mode reset.
    
    Change-Id: I0427782864570673702ff83efe8d7a727930b1fb

--- msm8994.mk
+++ msm8994.mk
@@ -37,7 +37,6 @@ PRODUCT_BRAND := Android
 PRODUCT_MODEL := MSM8994 for arm64
 
 PRODUCT_BOOT_JARS += tcmiface
-PRODUCT_BOOT_JARS += com.qti.dpmframework
 ifneq ($(strip $(QCPATH)),)
 PRODUCT_BOOT_JARS += qcom.fmradio
 PRODUCT_BOOT_JARS += WfdCommon
@@ -45,6 +44,7 @@ PRODUCT_BOOT_JARS += extendedmediaextractor
 PRODUCT_BOOT_JARS += security-bridge
 PRODUCT_BOOT_JARS += qsb-port
 PRODUCT_BOOT_JARS += oem-services
+PRODUCT_BOOT_JARS += com.qti.dpmframework
 endif
 
 #Android EGL implementation
