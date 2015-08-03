783648a6eead3bfe3c0d327f356fd1e6bb081f18
dianlujitao <dianlujitao@gmail.com>
2015-08-03 09:55:27 +0800

    Revert "nx510j: Set selinux to permissive"
    This reverts commit af113ca22e26df2a7f741f7c93ac4cd935b20b59.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,7 +129,7 @@ TARGET_INIT_VENDOR_LIB := libinit_msm
 
 # Kernel
 TARGET_NO_KERNEL := false
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
