31de8819f7ef77d4d2f8fde5787ec1128f772c36
dianlujitao <dianlujitao@gmail.com>
2015-08-28 13:57:36 +0800

    nx510j: Add radio images
     * From official Android 5.1.1 beta352
    
    Change-Id: I0aa1f5d45668f49c1627d1235edc8c2baa2965b4

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -114,6 +114,29 @@ $(shell mkdir -p $(TARGET_OUT_ETC)/firmware/wlan/qca_cld; \
 endif
 
 #----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+
+TARGET_BOOTLOADER_EMMC_INTERNAL := $(LOCAL_PATH)/images/emmc_appsboot.mbn
+$(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
+
+INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
+$(call add-radio-file,images/NON-HLOS.bin)
+$(call add-radio-file,images/BTFM.bin)
+$(call add-radio-file,images/rpm.mbn)
+$(call add-radio-file,images/sbl1.mbn)
+$(call add-radio-file,images/sdi.mbn)
+$(call add-radio-file,images/tz.mbn)
+$(call add-radio-file,images/hyp.mbn)
+$(call add-radio-file,images/pmic.mbn)
+endif
+
+#----------------------------------------------------------------------
 # ultrasound support
 #----------------------------------------------------------------------
 include $(CLEAR_VARS)
