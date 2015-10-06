abd11ee0708351fe7a7a44b1a71d8aa74a4b5984
dianlujitao <dianlujitao@gmail.com>
2015-10-06 18:28:28 +0800

    nx510j: Add missing permission for low latency output
    Change-Id: I9d4a88e062041a284df7f422f8f8d0f148211818

--- nx510j.mk
+++ nx510j.mk
@@ -56,7 +56,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml
 
 # MSM IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \
