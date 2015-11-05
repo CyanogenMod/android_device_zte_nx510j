1a35e4defbf8cd27c26bc7b8061bfaf71b094839
dianlujitao <dianlujitao@gmail.com>
2015-11-05 16:11:15 +0800

    nx510j: Force camera2.portability api
    Change-Id: I140b9f732e23d4921db547539ba08fdb15042278

--- nx510j.mk
+++ nx510j.mk
@@ -367,6 +367,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     audio_hal.period_size=192
 
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera2.portability.force_api=1
+
 # For debugging
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.secure=0 \
