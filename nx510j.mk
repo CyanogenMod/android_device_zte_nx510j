05911c27dacba2bda0349577802c408d7d1455a7
dianlujitao <dianlujitao@gmail.com>
2015-11-11 16:08:24 +0800

    nx510j: Add audio_output_policy.conf back
    Change-Id: Id950dcb3cf615848592f86119b1953fdcddb9e3a

--- nx510j.mk
+++ nx510j.mk
@@ -33,6 +33,7 @@ PRODUCT_COPY_FILES += \
 # Audio configuration file
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio_platform_info.xml:system/etc/audio_platform_info.xml
