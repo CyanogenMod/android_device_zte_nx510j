df87f14998bc4737565987fb86195d1a4aeca427
dianlujitao <dianlujitao@gmail.com>
2015-11-25 20:04:15 +0800

    nx510j: Update camera shim library
    Change-Id: I586b8aa88381c380f337c0c063c9a3dee0bed34f

--- camera/zte_camera.c
+++ camera/zte_camera.c
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "ZTE_CAM_SHIM"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -25,69 +24,70 @@
 
 //various funcs we'll need to call, in their mangled form
 
-//android::String8::String8(char const*)
-extern void _ZN7android7String8C1EPKc(void **str8P, const char *str);
+    //android::String8::String8(char const*)
+    extern void _ZN7android7String8C1EPKc(void **str8P, const char *str);
 
-//android::String8::~String8()
-extern void _ZN7android7String8D1Ev(void **str8P);
+    //android::String8::~String8()
+    extern void _ZN7android7String8D1Ev(void **str8P);
 
-//android::String16::String16(char const*)
-extern void _ZN7android8String16C1EPKc(void **str16P, const char *str);
+    //android::String16::String16(char const*)
+    extern void _ZN7android8String16C1EPKc(void **str16P, const char *str);
 
-//android::String16::~String16()
-extern void _ZN7android8String16D1Ev(void **str16P);
+    //android::String16::~String16()
+    extern void _ZN7android8String16D1Ev(void **str16P);
 
-//android::SensorManager::~SensorManager()
-extern void _ZN7android13SensorManagerD1Ev(void *sensorMgr);
+    //android::SensorManager::~SensorManager()
+    extern void _ZN7android13SensorManagerD1Ev(void *sensorMgr);
 
-//android::SensorManager::SensorManager(android::String16 const&)
-extern void _ZN7android13SensorManagerC1ERKNS_8String16E(void *sensorMgr, void **str16P);
+    //android::SensorManager::SensorManager(android::String16 const&)
+    extern void _ZN7android13SensorManagerC1ERKNS_8String16E(void *sensorMgr, void **str16P);
 
-//android::SensorManager::createEventQueue(android::String8, int)
-extern void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(void **retVal, void *sensorMgr, void **str8P, int mode);
+    //android::SensorManager::createEventQueue(android::String8, int)
+    extern void _ZN7android13SensorManager16createEventQueueENS_7String8Ei(void **retVal, void *sensorMgr, void **str8P, int mode);
 
 
 //data exports we must provide for camera library to be happy
 
-/*
- * DATA:     android::Singleton<android::SensorManager>::sLock
- * USE:      INTERPOSE: a mutes that camera lib will insist on accessing
- * NOTES:    In L, the sensor manager exposed this lock that callers
- *           actually locked & unlocked when accessing it. In M this
- *           is no longer the case, but we still must provide it for
- *           the camera library to be happy. It will lock nothnhing, but
- *           as long as it is a real lock and pthread_mutex_* funcs
- *           work on it, the camera library will be happy.
- */
-pthread_mutex_t _ZN7android9SingletonINS_13SensorManagerEE5sLockE = PTHREAD_MUTEX_INITIALIZER;
+    /*
+     * DATA:     android::Singleton<android::SensorManager>::sLock
+     * USE:      INTERPOSE: a mutes that camera lib will insist on accessing
+     * NOTES:    In L, the sensor manager exposed this lock that callers
+     *           actually locked & unlocked when accessing it. In M this
+     *           is no longer the case, but we still must provide it for
+     *           the camera library to be happy. It will lock nothnhing, but
+     *           as long as it is a real lock and pthread_mutex_* funcs
+     *           work on it, the camera library will be happy.
+     */
+    pthread_mutex_t _ZN7android9SingletonINS_13SensorManagerEE5sLockE = PTHREAD_MUTEX_INITIALIZER;
 
-/*
- * DATA:     android::Singleton<android::SensorManager>::sInstance
- * USE:      INTERPOSE: a singleton instance of SensorManager
- * NOTES:    In L, the sensor manager exposed this variable, as it was
- *           a singleton and one could just access this directly to get
- *           the current already-existing instance if it happened to
- *           already exist. If not one would create one and store it
- *           there. In M this is entirely different, but the camera library
- *           does not know that. So we'll init it to NULL to signify that
- *           no current instance exists, let it create one, and store it
- *           here, and upon unloading we'll clean it up, if it is not
- *           NULL (which is what it would be if the camera library itself
- *           did the cleanup).
- */
-void* _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE = NULL;
+    /*
+     * DATA:     android::Singleton<android::SensorManager>::sInstance
+     * USE:      INTERPOSE: a singleton instance of SensorManager
+     * NOTES:    In L, the sensor manager exposed this variable, as it was
+     *           a singleton and one could just access this directly to get
+     *           the current already-existing instance if it happened to
+     *           already exist. If not one would create one and store it
+     *           there. In M this is entirely different, but the camera library
+     *           does not know that. So we'll init it to NULL to signify that
+     *           no current instance exists, let it create one, and store it
+     *           here, and upon unloading we'll clean it up, if it is not
+     *           NULL (which is what it would be if the camera library itself
+     *           did the cleanup).
+     */
+    void* _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE = NULL;
+
 
 //code exports we provide
 
-//android::SensorManager::SensorManager(void)
-void _ZN7android13SensorManagerC1Ev(void *sensorMgr);
+    //android::SensorManager::SensorManager(void)
+    void _ZN7android13SensorManagerC1Ev(void *sensorMgr);
 
-//android::SensorManager::createEventQueue(void)
-void _ZN7android13SensorManager16createEventQueueEv(void **retVal, void *sensorMgr);
+    //android::SensorManager::createEventQueue(void)
+    void _ZN7android13SensorManager16createEventQueueEv(void **retVal, void *sensorMgr);
 
 //library on-load and on-unload handlers (to help us set things up and tear them down)
-void libEvtLoading(void) __attribute__((constructor));
-void libEvtUnloading(void) __attribute__((destructor));
+    void libEvtLoading(void) __attribute__((constructor));
+    void libEvtUnloading(void) __attribute__((destructor));
 
 /*
  * FUNCTION: android::SensorManager::SensorManager(void)
@@ -96,13 +96,13 @@ void libEvtUnloading(void) __attribute__((destructor));
  *           in a package name as a "string16" to the consrtuctor. Since this
  *           lib only services camera library, it is easy for us to just do that
  *           and this provide the constructor that the camera library wants.
- *           The package name we use if "camera.msm8960". Why not?
+ *           The package name we use if "camera.msm8994". Why not?
  */
 void _ZN7android13SensorManagerC1Ev(void *sensorMgr)
 {
     void *string;
 
-    _ZN7android8String16C1EPKc(&string, "camera.m7");
+    _ZN7android8String16C1EPKc(&string, "camera.msm8994");
     _ZN7android13SensorManagerC1ERKNS_8String16E(sensorMgr, &string);
     _ZN7android8String16D1Ev(&string);
 }
@@ -132,7 +132,7 @@ void _ZN7android13SensorManager16createEventQueueEv(void **retVal, void *sensorM
  */
 void libEvtLoading(void)
 {
-    ALOGV("Sensor manager interposition library loaded");
+    ALOGI("msm8994 camera interposition library loaded. Your camera should work in M now.");
 }
 
 /*
@@ -142,10 +142,12 @@ void libEvtLoading(void)
  */
 void libEvtUnloading(void)
 {
-    ALOGV("Sensor manager interposition library unloading. Goodbye...");
+    ALOGI("msm8994 camera interposition library unloading. Goodbye...");
     if (_ZN7android9SingletonINS_13SensorManagerEE9sInstanceE) {
         //if an instance stil exists, free it by calling the destructor, just to be throrough
         _ZN7android13SensorManagerD1Ev(_ZN7android9SingletonINS_13SensorManagerEE9sInstanceE);
         _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE = NULL;
     }
 }
+
+void _ZN7android5Fence4waitEj() {}
