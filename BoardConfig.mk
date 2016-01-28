4801f47115a0e7233e6fb807981b30a91ca49ded
dianlujitao <dianlujitao@gmail.com>
2016-01-28 22:05:56 +0800

    nx510j: Add cmhw vibrator intensity control
    Change-Id: Id6e105b48dcd5d36cb01e3c1a428e009622097f4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,9 @@ BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CM Hardware
-BOARD_HARDWARE_CLASS := hardware/cyanogen/cmhw
+BOARD_HARDWARE_CLASS := \
+    hardware/cyanogen/cmhw \
+    $(LOCAL_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/data/tp/easy_wakeup_gesture"
 
 #Enable HW based full disk encryption
@@ -186,7 +188,7 @@ BOARD_USES_QCOM_HARDWARE := true
 ADD_RADIO_FILES := true
 
 # Recovery
-#RECOVERY_VARIANT := twrp
+RECOVERY_VARIANT := twrp
 ifneq ($(RECOVERY_VARIANT),twrp)
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
 else
@@ -202,7 +204,7 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 TW_TARGET_USES_QCOM_BSP := true
-#TW_BUILD_ZH_CN_SUPPORT := true
+TW_BUILD_ZH_CN_SUPPORT := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 endif
