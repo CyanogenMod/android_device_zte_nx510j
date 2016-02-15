1c0abb39f779b439dcab00d0872d80e83dac27d1
dianlujitao <dianlujitao@gmail.com>
2016-02-15 10:42:40 +0800

    Revert "nx510j: camera: Disable unsupported HDR mode"
     * Working now.
    
    This reverts commit 4f22a23a41a3f7a0a6adf55632833290ab6bcbc2.
    
    Change-Id: I183e2533cfbc97f7391d7d25e07a87b18ea5674c

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -99,8 +99,6 @@ static int check_vendor_module()
 
 static char *camera_fixup_getparams(int id, const char *settings)
 {
-    const char *supportedSceneModes = "auto,asd,landscape,snow,beach,sunset,night,portrait,backlight,sports,steadyphoto,flowers,candlelight,fireworks,party,night-portrait,theatre,action,AR";
-
     android::CameraParameters params;
     params.unflatten(android::String8(settings));
 
@@ -108,11 +106,6 @@ static char *camera_fixup_getparams(int id, const char *settings)
     ALOGV("%s: original parameters:", __FUNCTION__);
     params.dump();
 #endif
-
-    /* Set supported scene modes. Remove HDR scene mode. */
-    if (params.get(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES)) {
-        params.set(android::CameraParameters::KEY_SUPPORTED_SCENE_MODES, supportedSceneModes);
-    }
 
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
