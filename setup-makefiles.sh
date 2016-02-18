4ac6b921942f814a9f43dcdca0bd2e0e2edbb9b0
dianlujitao <dianlujitao@gmail.com>
2016-02-18 14:29:38 +0800

    nx510j: Update adreno blobs from sambar
    Change-Id: I16865d5fda7505720db569967ce1fa2588d3c135

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -72,6 +72,9 @@ PRODUCT_PACKAGES += \\
     libloc_api_v02 \\
     libloc_ds_api
 
+PRODUCT_PACKAGES += \\
+    libmm-disp-apis
+
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
 
@@ -173,6 +176,20 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_MULTILIB := both
+include \$(BUILD_PREBUILT)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libmm-disp-apis
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libmm-disp-apis.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libmm-disp-apis.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MULTILIB := both
+LOCAL_PROPRIETARY_MODULE := true
 include \$(BUILD_PREBUILT)
 
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
