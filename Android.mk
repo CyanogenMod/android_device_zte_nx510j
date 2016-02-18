9253f99f8cb4f34a21e1c2fd8f6d8fbee5d18a34
dianlujitao <dianlujitao@gmail.com>
2016-02-18 11:35:25 +0800

    [WIP]nx510j: Restructure & clean up device tree
    Change-Id: I73fb58e8ba17885042023f2027cc0d16262823aa

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,6 +25,42 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter nx510j,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),nx510j)
+
+#----------------------------------------------------------------------
+# Radio image
+#----------------------------------------------------------------------
+ifeq ($(ADD_RADIO_FILES), true)
+radio_dir := $(LOCAL_PATH)/radio
+RADIO_FILES := $(shell cd $(radio_dir) ; ls)
+$(foreach f, $(RADIO_FILES), \
+    $(call add-radio-file,radio/$(f)))
+
+TARGET_BOOTLOADER_EMMC_INTERNAL := $(LOCAL_PATH)/images/emmc_appsboot.mbn
+$(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
+
+INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
+$(call add-radio-file,images/NON-HLOS.bin)
+$(call add-radio-file,images/BTFM.bin)
+$(call add-radio-file,images/rpm.mbn)
+$(call add-radio-file,images/sbl1.mbn)
+$(call add-radio-file,images/sdi.mbn)
+$(call add-radio-file,images/tz.mbn)
+$(call add-radio-file,images/hyp.mbn)
+$(call add-radio-file,images/pmic.mbn)
+$(call add-radio-file,images/splash.img)
+endif
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+include $(CLEAR_VARS)
+
+# Create a link for the WCNSS config file, which ends up as a writable
+# version in /data/misc/wifi
+$(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/qca_cld; \
+    ln -sf /system/etc/wifi/WCNSS_qcom_cfg.ini \
+	    $(TARGET_OUT)/etc/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini; \
+    ln -sf /persist/wlan_mac.bin \
+	    $(TARGET_OUT_ETC)/firmware/wlan/qca_cld/wlan_mac.bin)
+
 endif
