4ec2b93b74c0da63cd2ec178e47c1826c2d3bb55
Keith Mok <kmok@cyngn.com>
2016-03-03 15:32:08 -0800

    nx510j: Completely remove QC location stack
    * liblbs_core checks for existence of com.qualcomm.location to determine
      whether location injection occurs.
    
    CYNGNOS-1514
    
    Change-Id: I7ee8987789efd2e7741963b7fff3877a9c0f569d

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -68,7 +68,6 @@ done
 PRODUCT_PACKAGES += \\
     TimeService \\
     libtime_genoff \\
-    com.qualcomm.location \\
     libloc_api_v02 \\
     libloc_ds_api
 
@@ -147,17 +146,6 @@ LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MULTILIB := both
 LOCAL_PROPRIETARY_MODULE := true
-include \$(BUILD_PREBUILT)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
 include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
