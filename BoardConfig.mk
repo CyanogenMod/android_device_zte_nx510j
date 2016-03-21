d83f929dd3882c3ae7651eacd564b3ebdea497c2
dianlujitao <dianlujitao@gmail.com>
2016-03-21 12:03:37 +0800

    Revert "nx510j: Disable dexpreopt for now"
    This reverts commit 4113d0adb3899c4651d771ab85c3617ac0b85145.
    
    Conflicts:
    	BoardConfig.mk
    
    Change-Id: Ia2831c4e49cc41cf6545d848b6b24e32dd2a50ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -126,6 +126,18 @@ TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
 BOARD_USES_OPENSSL_SYMBOLS := true
 
+# Enable dex pre-opt to speed up initial boot
+ifeq ($(HOST_OS),linux)
+  ifeq ($(WITH_DEXPREOPT),)
+    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_PIC := true
+    ifneq ($(TARGET_BUILD_VARIANT),user)
+      # Retain classes.dex in APK's for non-user builds
+      DEX_PREOPT_DEFAULT := nostripping
+    endif
+  endif
+endif
+
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
 
