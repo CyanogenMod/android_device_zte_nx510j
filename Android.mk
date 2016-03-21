9ae93e6fd41e0f2a26d943b1f1e051f919fed223
dianlujitao <dianlujitao@gmail.com>
2016-03-21 09:16:06 +0800

    [DO MOT MERGE]nx510j: Temporary remove radio image
    Change-Id: Iedd999535408c6eeae1bc64d4a0533390976b206

--- Android.mk
+++ Android.mk
@@ -27,30 +27,6 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),nx510j)
 
-#----------------------------------------------------------------------
-# Radio image
-#----------------------------------------------------------------------
-ifeq ($(ADD_RADIO_FILES), true)
-radio_dir := $(LOCAL_PATH)/radio
-RADIO_FILES := $(shell cd $(radio_dir) ; ls)
-$(foreach f, $(RADIO_FILES), \
-    $(call add-radio-file,radio/$(f)))
-
-TARGET_BOOTLOADER_EMMC_INTERNAL := $(LOCAL_PATH)/images/emmc_appsboot.mbn
-$(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
-
-INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
-$(call add-radio-file,images/NON-HLOS.bin)
-$(call add-radio-file,images/BTFM.bin)
-$(call add-radio-file,images/rpm.mbn)
-$(call add-radio-file,images/sbl1.mbn)
-$(call add-radio-file,images/sdi.mbn)
-$(call add-radio-file,images/tz.mbn)
-$(call add-radio-file,images/hyp.mbn)
-$(call add-radio-file,images/pmic.mbn)
-$(call add-radio-file,images/splash.img)
-endif
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
--- releasetools.py
+++ releasetools.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Emit commands needed for ZTE devices during OTA installation
-(installing the aboot/modem/rpm/sbl/tz/bluetooth/hyp/pmic images)."""
-
 import common
 import re
 import sha
@@ -26,46 +23,6 @@ def FullOTA_Assertions(info):
 def IncrementalOTA_Assertions(info):
   AddBasebandAssertion(info)
   return
-
-def InstallImage(img_name, img_file, partition, info):
-  common.ZipWriteStr(info.output_zip, img_name, img_file)
-  info.script.AppendExtra(('package_extract_file("' + img_name + '", "/dev/block/bootdevice/by-name/' + partition + '");'))
-
-image_partitions = {
-   'emmc_appsboot.mbn' : 'aboot',
-   'rpm.mbn'           : 'rpm',
-   'tz.mbn'            : 'tz',
-   'hyp.mbn'           : 'hyp',
-   'sdi.mbn'           : 'sdi',
-   'pmic.mbn'          : 'pmic',
-   'BTFM.bin'          : 'bluetooth',
-   'NON-HLOS.bin'      : 'modem',
-   'splash.img'        : 'splash',
-   'sbl1.mbn'          : 'sbl1'
-}
-
-def FullOTA_InstallEnd(info):
-  info.script.Print("Writing radio image...")
-  for k, v in image_partitions.iteritems():
-    try:
-      img_file = info.input_zip.read("RADIO/" + k)
-      info.script.Print("update image " + k + "...")
-      InstallImage(k, img_file, v, info)
-    except KeyError:
-      print "warning: no " + k + " image in input target_files; not flashing " + k
-
-
-def IncrementalOTA_InstallEnd(info):
-  for k, v in image_partitions.iteritems():
-    try:
-      source_file = info.source_zip.read("RADIO/" + k)
-      target_file = info.target_zip.read("RADIO/" + k)
-      if source_file != target_file:
-        InstallImage(k, target_file, v, info)
-      else:
-        print k + " image unchanged; skipping"
-    except KeyError:
-      print "warning: " + k + " image missing from target; not flashing " + k
 
 def AddBasebandAssertion(info):
   android_info = info.input_zip.read("OTA/android-info.txt")
