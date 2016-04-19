c886c277c7f9f090e14b8643dace9dcafaa4bae0
dianlujitao <dianlujitao@gmail.com>
2016-04-19 12:44:50 +0800

    nx510j: Drop to lolipop RIL/QMI stack
     * Official M RIL/baseband can't work properly with CM, drop to
       L baseband/RIL/QMI for now until I find a solution.
     * Use modified radio image: M radio firmware with L baseband.
     * Disable EAP-SIM support, it doesn't work with our L RIL/QMI stack.
    
    Change-Id: Id33ea75b22d26d3b86a234774ef7430602eadf89

--- device.mk
+++ device.mk
@@ -224,7 +224,7 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/telephony/NX510J_Z0.xml:system/etc/telephony/NX510J_Z0.xml \
+    $(LOCAL_PATH)/configs/telephony/NX510J_Z0-0.xml:system/etc/telephony/NX510J_Z0-0.xml \
     $(LOCAL_PATH)/configs/telephony/default_static_config.xml:system/etc/telephony/default_static_config.xml
 
 # Sensors
