e5571c70093c387cc39dbddb1ad7e38143469eaa
dianlujitao <dianlujitao@gmail.com>
2016-04-21 00:25:41 +0800

    [WIP]nx510j: Move radioo images to vendor
    Change-Id: I1371f28b1d01a89df3f145b672ece637eb5d06f2

--- Android.mk
+++ Android.mk
@@ -31,24 +31,24 @@ ifeq ($(TARGET_DEVICE),nx510j)
 # Radio image
 #----------------------------------------------------------------------
 ifeq ($(ADD_RADIO_FILES), true)
-radio_dir := $(LOCAL_PATH)/radio
+radio_dir := $(TOP)/vendor/zte/nx510j/radio
 RADIO_FILES := $(shell cd $(radio_dir) ; ls)
 $(foreach f, $(RADIO_FILES), \
     $(call add-radio-file,radio/$(f)))
 
-TARGET_BOOTLOADER_EMMC_INTERNAL := $(LOCAL_PATH)/images/emmc_appsboot.mbn
+TARGET_BOOTLOADER_EMMC_INTERNAL := $(radio_dir)/emmc_appsboot.mbn
 $(TARGET_BOOTLOADER_EMMC_INTERNAL): $(TARGET_BOOTLOADER)
 
 INSTALLED_RADIOIMAGE_TARGET += $(TARGET_BOOTLOADER_EMMC_INTERNAL)
-$(call add-radio-file,images/NON-HLOS.bin)
-$(call add-radio-file,images/BTFM.bin)
-$(call add-radio-file,images/rpm.mbn)
-$(call add-radio-file,images/sbl1.mbn)
-$(call add-radio-file,images/sdi.mbn)
-$(call add-radio-file,images/tz.mbn)
-$(call add-radio-file,images/hyp.mbn)
-$(call add-radio-file,images/pmic.mbn)
-$(call add-radio-file,images/splash.img)
+$(call add-radio-file,$(radio_dir)/NON-HLOS.bin)
+$(call add-radio-file,$(radio_dir)/BTFM.bin)
+$(call add-radio-file,$(radio_dir)/rpm.mbn)
+$(call add-radio-file,$(radio_dir)/sbl1.mbn)
+$(call add-radio-file,$(radio_dir)/sdi.mbn)
+$(call add-radio-file,$(radio_dir)/tz.mbn)
+$(call add-radio-file,$(radio_dir)/hyp.mbn)
+$(call add-radio-file,$(radio_dir)/pmic.mbn)
+$(call add-radio-file,$(radio_dir)/splash.img)
 endif
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
