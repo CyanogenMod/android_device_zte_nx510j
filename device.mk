97ec693753d3ed871e0a6f2c9758673cef92cb69
dianlujitao <dianlujitao@gmail.com>
2016-06-12 00:50:47 +0800

    nx510j: Update GPS HAL from ether
    Change-Id: I1579c95d7a4224761939962443abf693944b170c

--- device.mk
+++ device.mk
@@ -134,12 +134,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gps.msm8994
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+PRODUCT_PACKAGES += \
+    flp.conf \
+    gps.conf \
+    izat.conf \
+    lowi.conf \
+    sap.conf
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.gps.qc_nlp_in_use=0 \
+    ro.gps.agps_provider=1
 
 # IPv6
 PRODUCT_PACKAGES += \
