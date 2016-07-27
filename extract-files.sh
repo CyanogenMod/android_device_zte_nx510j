3fcd42db9458a871ca4bbbb84036339e2f2a4adb
dianlujitao <dianlujitao@gmail.com>
2016-07-27 11:37:38 +0800

    nx510j: Switch to common firmware extraction method
    Change-Id: I9a27cfa3271d5a7e6d30dd65198740b7b0402030

--- extract-files.sh
+++ extract-files.sh
@@ -20,8 +20,6 @@ set -e
 DEVICE=nx510j
 VENDOR=zte
 
-export RADIO_IMAGES="BTFM.bin emmc_appsboot.mbn hyp.mbn NON-HLOS.bin pmic.mbn rpm.mbn sbl1.mbn sdi.mbn splash.img tz.mbn"
-
 # Load extractutils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
@@ -46,27 +44,12 @@ else
   else
     echo "$0: bad number of arguments"
     echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM] (PATH_TO_EXPANDED_RADIO)"
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM] (PATH_TO_RADIO_FOLDER)"
     echo ""
     echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
     echo "the device using adb pull."
     exit 1
   fi
-fi
-
-function extract_radio() {
-    if [ ! -d $1 ]; then
-        mkdir -p $1
-    fi
-    for FILE in $RADIO_IMAGES; do
-	      cp $RADIO_SRC/$FILE $1/$FILE
-    done
-}
-
-if [ -n "$RADIO_SRC" ]; then
-    RADIO_BASE=../../../vendor/$VENDOR/$DEVICE/radio
-    rm -rf $RADIO_BASE/*
-    extract_radio $RADIO_BASE
 fi
 
 # Initialize the helper
@@ -75,5 +58,8 @@ setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
 extract "$MY_DIR"/proprietary-files-qc.txt "$SRC"
 extract "$MY_DIR"/proprietary-files-qc-perf.txt "$SRC"
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
+if [ -n "$RADIO_SRC" ]; then
+    extract_firmware "$MY_DIR"/proprietary-firmware.txt "$RADIO_SRC"
+fi
 
 "$MY_DIR"/setup-makefiles.sh
